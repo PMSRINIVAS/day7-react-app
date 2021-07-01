548e25318b0592290d8f4a7d12ff332f9ceb43e7
PMSRINIVAS <killermaddy55@gmail.com>
2021-07-01 13:14:11 +0530

    static todo, onclick of button

--- src/App.js
+++ src/App.js
@@ -3,13 +3,40 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  //ARRAY DESTRUCTED
+  let [list, setList] = useState(["delhi"]);
+
+  let addPost = () => {
+    console.log("Add Post Method");
+    //ARRAY CLONING
+    const newList = ["New To-Do", ...list];
+    setList(newList);
+  };
+
   return (
     <div>
-      <h1 className="bg-primary text-light p-3">Bootstrap Demo</h1>
-      <p className="text-primary">
-        lorem, sdhjds xsbnsb sjbnsbn xbnbzhsb xsbjsxzbc xbmczbn zbnxzbmz
-        aujkdskjm xskxbmzvx
-      </p>
+      <h2 className="bg-dark text-light p-3">Mini Book</h2>
+
+      <div className="row">
+        <div className="col-9">
+          <input type="text" className="form-control form-control-lg" />
+        </div>
+
+        <div className="col-3">
+          <input
+            type="button"
+            className="btn btn-lg btn-primary"
+            value="Add To-Do"
+            onClick={addPost}
+          />
+        </div>
+      </div>
+
+      {list.map((item, index) => (
+        <div key={index} className="alert alert-secondary my-2">
+          {item}
+        </div>
+      ))}
     </div>
   );
 }
