cd47b469c29b27e4c996b00de001d7f290fa28fa
PMSRINIVAS <killermaddy55@gmail.com>
2021-07-01 19:48:07 +0530

    ToDo App with styling and design

--- src/App.js
+++ src/App.js
@@ -20,24 +20,42 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Working with the forms</h1>
+    <div className="bg-secondary">
+      <h1></h1>
+      <h1 className="bg-success text-dark p-3">
+        {" "}
+        📃 Working with the forms 📝
+      </h1>
 
-      <div>
-        <input
-          type="text"
-          value={newtodo}
-          onChange={updateTodo}
-          placeholder="ENTER YOUR TASK"
-        />
-        <input type="button" value="Add Todo" onClick={addTodo} />
+      <div className="row">
+        <div className="col-9">
+          <input
+            type="text"
+            value={newtodo}
+            onChange={updateTodo}
+            placeholder="ENTER YOUR TASK 🖊️"
+            className="form-control form-control-lg"
+          />
+        </div>
+        <div className="col-3">
+          <input
+            type="button"
+            className="btn btn-lg btn-primary text-white"
+            value="Add Todo"
+            onClick={addTodo}
+          />
+        </div>
       </div>
 
       {todoList.map((item, index) => (
-        <h3 key={index}>{item}</h3>
+        <h3 key={index} className="bg-danger text-white p-3 my-2">
+          ✍️{item}
+        </h3>
       ))}
 
-      {todoList.length == 0 && <div>No Todo List</div>}
+      {todoList.length == 0 && (
+        <div className="alert alert-success my-1">No Todo List ☮️</div>
+      )}
     </div>
   );
 }
