25b55bce9399956a7f81540e3e798f37bd5a0ef2
PMSRINIVAS <killermaddy55@gmail.com>
2021-07-01 12:17:57 +0530

    added the bootstrap.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  //Its recommended for inline styling
-  let mystyle = { fontSize: 32, backgroundColor: "red" };
-
   return (
     <div>
-      <h1>External Styling</h1>
-      <p className="primary">lorem sash dvhnds AJBAB</p>
-
-      <h1>Inline Styling in React</h1>
-      <p style={{ fontSize: 32, color: "tomato" }}>
-        lorem gsjamba xaghjsa xaghsnb --//this approach is not
-        recommended(inline css styling.)
+      <h1 className="bg-primary text-light p-3">Bootstrap Demo</h1>
+      <p className="text-primary">
+        lorem, sdhjds xsbnsb sjbnsbn xbnbzhsb xsbjsxzbc xbmczbn zbnxzbmz
+        aujkdskjm xskxbmzvx
       </p>
-      {/**Style Using the Object */}
-      <p style={mystyle}>lorem gsjamba xaghjsa xaghsnb </p>
     </div>
   );
 }
